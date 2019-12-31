e57b4052601cb6321620657fbaf9151a9b485b16
aparcanapavel <aparcanapavel@gmail.com>
2019-12-30 20:06:23 -0500

    modal working perfectly

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-function setup() {
-  createCanvas(640, 480);
-}
+// function setup() {
+//   createCanvas(640, 480);
+// }
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
-}
+// function draw() {
+//   if (mouseIsPressed) {
+//     fill(0);
+//   } else {
+//     fill(255);
+//   }
+//   ellipse(mouseX, mouseY, 80, 80);
+// }
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("loaded");
+  const modalContainer = document.getElementById("modal-container");
+  const continueBtn = document.getElementById("continue-button");
+
+  [modalContainer, continueBtn].forEach(element => {
+    element.addEventListener("click", (e) => {
+      modalContainer.remove();
+    })
+  })
+
+  
+})
